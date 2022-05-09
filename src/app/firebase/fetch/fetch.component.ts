7c11cd7ce0dd06c90f809798aeb7bfe3ed05160c
Jaime Guevarra <jaimeguevarra22@gmail.com>
2022-05-09 15:18:18 +1000

    Generic Type with HttpClient

--- src/app/firebase/fetch/fetch.component.ts
+++ src/app/firebase/fetch/fetch.component.ts
@@ -1,21 +1,25 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
-
+import { Post } from 'src/app/post.model';
+ 
 @Component({
   selector: 'app-fetch',
   templateUrl: './fetch.component.html',
   styleUrls: ['./fetch.component.css']
 })
 export class FetchComponent implements OnInit {
+  loadPosts = [];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.fetchPosts();
+    this.loadPosts;
   }
 
-  onCreatePost(postData: {title: string, content: string}){
+  onCreatePost(postData: Post){
     this.http
     .post('https://ngfirebase-enedb-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
     .subscribe((response)=>{ 
@@ -32,8 +36,19 @@ export class FetchComponent implements OnInit {
   }
 
   private fetchPosts(){
-    this.http.get('https://ngfirebase-enedb-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+    this.http.get<{[key: string]: Post}>('https://ngfirebase-enedb-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+    .pipe(map(responseData => {
+      const postArray: Post[] = [];
+      for(const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+        postArray.push({id : key, ...responseData[key]});
+        }
+      }
+
+      return postArray;
+    }))
     .subscribe(posts => {
+      //   ....
       console.log(posts);
     });
   }
