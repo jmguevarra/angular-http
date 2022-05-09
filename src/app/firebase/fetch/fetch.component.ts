36b3a70ab37f3c5b05d3ad5c61cdaa1ef25ba9f3
Jaime Guevarra <jaimeguevarra22@gmail.com>
2022-05-09 14:49:05 +1000

    Fetch Data

--- src/app/firebase/fetch/fetch.component.ts
+++ src/app/firebase/fetch/fetch.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-fetch',
@@ -11,21 +12,30 @@ export class FetchComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-
+    this.fetchPosts();
   }
 
   onCreatePost(postData: {title: string, content: string}){
-    this.http.post('https://ngfirebase-enedb-default-rtdb.asia-southeast1.firebasedatabase.app/', postData);
-
-    console.log()
+    this.http
+    .post('https://ngfirebase-enedb-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
+    .subscribe((response)=>{ 
+      console.log(response);
+    });
   }
 
   onFetchPosts(){
-
+    this.fetchPosts()
   }
 
   onClearPosts(){
     
+  }
+
+  private fetchPosts(){
+    this.http.get('https://ngfirebase-enedb-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+    .subscribe(posts => {
+      console.log(posts);
+    });
   }
 
 }
